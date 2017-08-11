dd3baa308719f7bbee2109677022ad7d9212121e
Julian <julianschroeter@outlook.com>
2017-08-11 10:10:32 +0200

    added interface for press

--- src/test/example/InheritancePolymorphism/OlivePress.java
+++ src/test/example/InheritancePolymorphism/OlivePress.java
@@ -4,16 +4,24 @@ import java.util.List;
 
 import test.example.InheritancePolymorphism.model.Olive;
 
-public class OlivePress {
+public class OlivePress implements Press{
+	
+	private int currentOil;
 	
 	public int getOil(List<Olive> olives) {
-		int totalOil = 0 ;
+		int totalOil = currentOil ;
 		for (Olive o : olives) {
 			System.out.println(o.getName());
 			totalOil += o.crush();
 			
 		}
 		return totalOil;
+		
+	}
+
+	@Override
+	public void setOil(int oil) {
+		currentOil=oil;
 		
 	}
 }
---
+++ src/test/example/InheritancePolymorphism/Press.java
@@ -0,0 +1,11 @@
+package test.example.InheritancePolymorphism;
+
+import java.util.List;
+
+import test.example.InheritancePolymorphism.model.Olive;
+
+public interface Press {
+	public int getOil(List<Olive> olives);
+	public void setOil(int oil);
+	
+}
