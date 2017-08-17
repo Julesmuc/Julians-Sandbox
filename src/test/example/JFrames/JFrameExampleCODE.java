e0c2cb07ee39892d01df3846bfc31c7ba20f5454
Julian <julianschroeter@outlook.com>
2017-08-17 12:13:45 +0200

    added example for jframe

---
+++ src/test/example/JFrames/JFrameExampleCODE.java
@@ -0,0 +1,14 @@
+package test.example.JFrames;
+
+import javax.swing.JFrame;
+
+public class JFrameExampleCODE {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrameExampleGUI();
+		frame.setTitle("Register form");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
